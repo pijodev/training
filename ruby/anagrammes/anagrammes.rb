8ffe144638246f0325f983d00d78be36bfd9a3a2
ILO <ilozano.jds@gmail.com>
2020-02-20 21:58:29 +0100

    PIDEV Ajout des sources pour anagrames

---
+++ ruby/anagrammes/anagrammes.rb
@@ -0,0 +1,8 @@
+puts "---------------------------"
+puts "         ANAGRAMMES"
+puts "---------------------------"
+
+puts "Il faut donner en entrée un anagramme d'un des mots présents dans fr.txt, à donner également en paramètre"
+puts ""
+puts "~~~~"
+puts ""
