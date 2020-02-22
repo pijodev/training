b971aeefd29204cc9fd38a2bed1f59eba61552b7
ILO <ilozano.jds@gmail.com>
2020-02-22 12:19:38 +0100

    PIDEV - Ajout des fichiers initialisant la resolution du pb sudoku

---
+++ ruby/sudoku/sudoku.rb
@@ -0,0 +1,10 @@
+puts "---------------------------"
+puts "         SUDOKU"
+puts "---------------------------"
+
+puts "Il faut donner en entrée un fichier .txt contenant une grille sudoku"
+puts ""
+puts "~~~~"
+puts ""
+
+
