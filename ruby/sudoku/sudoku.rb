d425b8829cba1b578b5ddbcc7523ab181b354ed8
ILO <ilozano.jds@gmail.com>
2020-02-25 00:14:54 +0100

    PIDEV-sudoku La fonction de transformToMultiDimArray fonctionne et retranscrit la grille en entree dans un tableau propre sans caracteres de separation parasites

--- ruby/sudoku/sudoku.rb
+++ ruby/sudoku/sudoku.rb
@@ -26,9 +26,9 @@ end
 sudoku = prepare(sudokuEntrant)
 puts sudoku 
 
-# Pour le moment, sert à afficher chaque case du tableau.
-# Cette méthode va créer un tableau à 2 dimensions, où une ligne vaut : [[], [], []]
-# On pourra ainsi étudier facilement le sudoku
+# Pour le moment, retourne la grille retranscrite en un tableau de 9 éléments, ou 1 élément est un string de 3 chiffres.
+# Cela permettra de repérer facilement la position de chaque "_" dans la grille.
+# Cette méthode va créer un tableau à 2 dimensions, où une ligne vaut : [[[], [], []],[[], [], []],[[], [], []]]
 def transformToMultiDimArray(sudoku)
     cpt_C = 0
     cpt_L = 0
@@ -38,22 +38,40 @@ def transformToMultiDimArray(sudoku)
 
     sudokuArr = []
 
-    caracSpec = ['|','_', '-', '+']
+    # Liste des caractères interdits : ils n'entravent pas la retranscription du sudoku.
+    caracSpec = ['|', '-', '+']
+    strike = 0
 
+    # Parcourir le tableau brut contenant le sudoku tel quel
     while cpt_L < nb_lin
+        tmpLine = []
+        tmpThird = ""
+        tmpThird = ""
         while cpt_C < nb_col
-            if (caracSpec.include?(sudoku[cpt_L][cpt_C].to_s))
-                puts("Caractère #{sudoku[cpt_L][cpt_C].to_s}")
-            else
-                puts("Nombre #{sudoku[cpt_L][cpt_C].to_s}")
+            # Ignorer les séparateurs et ajouter uniquement les chiffres par groupes de 3 ( à l'image de la grille originale dans le txt)
+            if !(caracSpec.include?(sudoku[cpt_L][cpt_C].to_s))
+                tmpThird.concat(sudoku[cpt_L][cpt_C].to_s)
+                strike += 1
+            end
+            # On pousse les chiffres par groupes de 3 dans le tableau multi-dim
+            if strike == 3
+                strike = 0
+                tmpLine.push(tmpThird)
+                tmpThird = ""
             end
 
             cpt_C += 1
         end
-        puts("")
+        # 1 ligne contient donc 3 groupes de 3 chiffres soit le total des 9 chiffres / ligne
+        sudokuArr.push(tmpLine)
         cpt_C = 0
         cpt_L += 1
     end
+    return sudokuArr 
 end
 
 transformToMultiDimArray(sudoku)
+sudokuArr = transformToMultiDimArray(sudoku)
+p sudokuArr
+
+puts (" >>> Place à la résolution traitement <<< ")
