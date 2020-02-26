338cfd6c9b84bec095509ef26c5b130da3f6e102
ILO <ilozano.jds@gmail.com>
2020-02-26 21:52:20 +0100

    PIDEV-sudoku Ajout de la methode calculateInsideLine qui est basee sur celle pour calculer dans les carres - calcule la valeur du tiret contenu dans la ligne

--- ruby/sudoku/sudoku.rb
+++ ruby/sudoku/sudoku.rb
@@ -172,7 +172,9 @@ def calculateInsideSquare(linePos, colPos, strPos, sudokuArr)
             end
         end
     end
-    return sudokuArr
+    # TODO : vérifier que cette condition permet bien de sortir de la boucle du main. Ca servirait à éviter les potentielles collisions
+    # ou alors renvoyer un boolean et n'activer les fonctions calculate uniquement que si le boolean est à false
+    # break
 end
 
 
@@ -184,8 +186,24 @@ end
 ######################################
 
 
-def calculateInsideLine(i, k, sudokuArr[i][k].index('_'), sudokuArr)
-    puts("TODO")
+def calculateInsideLine(linePos, colPos, strPos, sudokuArr)
+
+    tmp = ""
+    for i in 0..sudokuArr[linePos].length()-1
+        puts sudokuArr[linePos][i]
+        tmp.concat(sudokuArr[linePos][i])
+    end
+    p tmp
+    if tmp.scan(/_/).length() == 1
+        for k in 1..9
+            if !tmp.include?(k.to_s)
+                puts("I found the missing ! #{k}")
+                # TODO : trouver le moyen de remplacer le tiret aux coordonnées indiquées par la valeur calculée
+                sudokuArr[linePos][colPos].tr!("_", k.to_s)
+                puts sudokuArr[linePos][colPos]
+            end
+        end
+    end
 end
 
 
@@ -207,9 +225,12 @@ def main(sudokuArr)
             # p sudokuArr[i][k]
             if sudokuArr[i][k].include?("_")
                 calculateInsideLine(i, k, sudokuArr[i][k].index('_'), sudokuArr)
-                calculateInsideSquare(i, k, sudokuArr[i][k].index('_'), sudokuArr)
+                # puts("&-&-&-&-&-&-&")
+                # FONCTIONNEL en solo mais pas en combinaison avec calculateInsideLine
+                # calculateInsideSquare(i, k, sudokuArr[i][k].index('_'), sudokuArr)
             end
         end
+        # puts ("&-&-&-&-&-&-&&-&-&-&-&-&-&")
     end
     puts ("------")
     p sudokuArr
