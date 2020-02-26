d433a22f5781cd6f6ab053d05744e8127e799d88
ILO <ilozano.jds@gmail.com>
2020-02-26 21:38:09 +0100

    PIDEV-sudoku Ajout des methodes permettant de trouver la valeur de _ dans les 9  carrés de 9 chiffres

--- ruby/sudoku/sudoku.rb
+++ ruby/sudoku/sudoku.rb
@@ -10,6 +10,13 @@ puts ""
 
 sudokuEntrant = ARGV[0]
 
+
+######################################
+#######                        #######
+#######          PREPARE       #######
+#######                        #######
+######################################
+
 # Prepare data
 def prepare(myFilePath)
     myFile = File.open(myFilePath)
@@ -70,8 +77,143 @@ def transformToMultiDimArray(sudoku)
     return sudokuArr 
 end
 
+puts (" >>> La grille de Sudoku a été transformée sous cette forme <<< ")
 transformToMultiDimArray(sudoku)
 sudokuArr = transformToMultiDimArray(sudoku)
+# Suppression de tableaux vides résiduels causés par transformToMultiDimArray
+sudokuArr.delete([])
 p sudokuArr
 
-puts (" >>> Place à la résolution traitement <<< ")
+puts ("")
+puts (" >>> Place à la résolution <<< ")
+puts ("")
+
+
+
+######################################
+#######                        #######
+#######     CALCULATE SQUARE   #######
+#######                        #######
+######################################
+
+
+# Crée la coordonnée en fonction de la ligne où se trouve le "_"
+def getSquareCoords(myPosition, coordSquare)
+    case myPosition
+    when 0..2
+        coordSquare.push(1)
+    when 3..5
+        coordSquare.push(2)
+    when 6..8
+        coordSquare.push(3)
+    end
+end
+# Calculateur des coordonnées du gros carré qui contient le tiret parcouru en ce moment
+def checkSquare(linePos, colPos, strPos)
+    puts ("Coordonnées du '_' : ligne #{linePos}, colonne #{colPos}, pos : #{strPos}")
+    coordSquare = []
+    # Créer les coordonnées du gros carré
+    getSquareCoords(linePos, coordSquare)
+    # getSquareCoords(colPos, coordSquare)
+    coordSquare.push(colPos)
+    # p coordSquare
+    return coordSquare
+
+end
+
+# Défini la range pour chaque carré - onse base sur des coordonnées pour chaque carré.
+# La coordonnée 1 est la ligne (de 0 à 8) et la coordonnée est la colonne de 3 (donc varie entre seulement 0, 1 ou 2) 
+# ex : carré central = [2, 1] : donc les lignes à récupérer sont les lignes 3 4 et 5 dans la grille, la colonne à récup est la numéro 1
+def defineRange(coord)
+    s_L_loop = 0
+    e_L_loop = 0
+
+    case coord
+    when [1,0], [1,1], [1,2]
+        s_L_loop = 0
+        e_L_loop = 2
+
+    when [2,0], [2,1], [2,2]
+        s_L_loop = 3
+        e_L_loop = 5
+
+    when [3,0], [3,1], [3,2]
+        s_L_loop = 6
+        e_L_loop = 8
+
+    end
+    
+    line_start_stop = []
+    line_start_stop.push(s_L_loop)
+    line_start_stop.push(e_L_loop)
+    return line_start_stop
+end
+
+# Regroupe en carrés de 9 chiffres
+def calculateInsideSquare(linePos, colPos, strPos, sudokuArr)
+    coordSquare = checkSquare(linePos, colPos, strPos)
+    p coordSquare 
+    line_start_stop = []
+    line_start_stop = defineRange(coordSquare)
+    tmp = ""
+    # Regrouper tous les éléments appartenant au gros carré en 1 ligne : si un seul "_" appartient au carré, alors on résoud la valeur manquante 
+    for i in line_start_stop[0]..line_start_stop[1]
+        puts sudokuArr[i][coordSquare[1]]
+        tmp.concat(sudokuArr[i][coordSquare[1]])
+    end
+    # Si il n'y a qu'un seul tiret à remplacer dans le carré, alors on y va
+    if tmp.scan(/_/).length() == 1
+        for k in 1..9
+            if !tmp.include?(k.to_s)
+                puts("I found the missing ! #{k}")
+                # TODO : trouver le moyen de remplacer le tiret aux coordonnées indiquées par la valeur calculée
+                sudokuArr[linePos][colPos].tr!("_", k.to_s)
+                puts sudokuArr[linePos][colPos]
+            end
+        end
+    end
+    return sudokuArr
+end
+
+
+
+######################################
+#######                        #######
+#######     CALCULATE LINE     #######
+#######                        #######
+######################################
+
+
+def calculateInsideLine(i, k, sudokuArr[i][k].index('_'), sudokuArr)
+    puts("TODO")
+end
+
+
+
+
+
+
+
+
+######################################
+#######                        #######
+#######          MAIN          #######
+#######                        #######
+######################################
+
+def main(sudokuArr)
+    for i in 0..sudokuArr.length()-1
+        for k in 0..sudokuArr[0].length()-1
+            # p sudokuArr[i][k]
+            if sudokuArr[i][k].include?("_")
+                calculateInsideLine(i, k, sudokuArr[i][k].index('_'), sudokuArr)
+                calculateInsideSquare(i, k, sudokuArr[i][k].index('_'), sudokuArr)
+            end
+        end
+    end
+    puts ("------")
+    p sudokuArr
+end
+
+
+main(sudokuArr)
